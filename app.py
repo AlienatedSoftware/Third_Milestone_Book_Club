0ce022738fe6667607ef9bcfd9a6c966edc1a873
AlienatedSoftware <aidancgart@gmail.com>
2021-10-26 22:57:45 +0000

    Implemented functionality forlogout system

--- app.py
+++ app.py
@@ -84,7 +84,18 @@ def profile(username):
     # Grabs the session user's username from database
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    # Removes the user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
